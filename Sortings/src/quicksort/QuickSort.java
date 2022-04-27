361aea126a1c6f6e066bea26306976267ec449a3
SoeThiHanKyaw <75842390+soethihankyaw@users.noreply.github.com>
2022-04-27 23:52:13 +0630

    QuickSort

--- Sortings/src/quicksort/QuickSort.java
+++ Sortings/src/quicksort/QuickSort.java
@@ -3,10 +3,19 @@ package quicksort;
 public class QuickSort {
 
 	public static void main(String[] args) {
+		int a[] = { 13, 18, 27, 2, 19, 25 };  
+	    int n = a.length;  
+	    
+	    System.out.println("Before sorting array elements are ");
+		printArray(a, n);
+		quick(a, 0, n -1);
+		
+		System.out.println("After sorting array elements are ");
+		printArray(a, n);
 		
 	}
 	
-	int partition (int a[], int start, int end)  
+	static int partition (int a[], int start, int end)  
 	{  
 	    int pivot = a[end]; 
 	    int i = (start - 1);  
@@ -26,4 +35,21 @@ public class QuickSort {
 	    a[end] = t;  
 	    return (i + 1);  
 	} 
+	
+	public static void quick(int a[], int start, int end) 
+	{  
+	    if (start < end)  
+	    {  
+	        int p = partition(a, start, end);  
+	        quick(a, start, p - 1);  
+	        quick(a, p + 1, end);  
+	    }  
+	}  
+	  
+	public static void printArray(int a[], int n)  
+	{  
+	    int i;  
+	    for (i = 0; i < n; i++)  
+	        System.out.print(a[i] + " ");  
+	}  
 }
